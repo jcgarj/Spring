01be7a2d4bb9f11c4a181c6ad1878bf38cea75bb
Jesus Quevedo <jenrique_qt@hotmail.com>
2019-05-23 12:40:37 -0500

    Ya se tienen las clases corriendo con jdbc template, falta el distinguir entre insert o update

--- src/main/java/mx/com/aion/data/controller/ClienteController.java
+++ src/main/java/mx/com/aion/data/controller/ClienteController.java
@@ -3,6 +3,8 @@ package mx.com.aion.data.controller;
 import mx.com.aion.data.models.dao.IClienteDao;
 import mx.com.aion.data.models.entity.Cliente;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -11,35 +13,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+
 import javax.validation.Valid;
 import java.util.Map;
-import java.util.Optional;
 
 @Controller
 @SessionAttributes("cliente")
 public class ClienteController {
 
+    private String sql = "select * from clientes";
+    private String sql2 = "select * from clientes where id = ?";
+    private String sql3 = "delete from clientes where id = ?";
+    private String sql4 = "insert into clientes(id, nombre, apellido, email, create_at) values (?,?,?,?,?)";
+    private String sql5 = "update clientes set nombre = ?, apellido = ?, email = ?, create_at = ? where id = ?";
+
     @Autowired
     private IClienteDao clienteService;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @RequestMapping(value = "/listar", method = RequestMethod.GET)
     public String listar(Model model){
         model.addAttribute("titulo", "Listado de clientes");
-        model.addAttribute("clientes", clienteService.findAll());
+        model.addAttribute("clientes", jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Cliente.class)));
         return "listar";
     }
 
     @RequestMapping(value = "/index")
     public String index(Model model){
         model.addAttribute("titulo", "Listado de clientes");
-        model.addAttribute("clientes", clienteService.findAll());
+        model.addAttribute("clientes", jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Cliente.class)));
         return "index";
     }
 
     @RequestMapping(value = "/login")
     public String login(Model model){
         model.addAttribute("titulo", "Listado de clientes");
-        model.addAttribute("clientes", clienteService.findAll());
+        model.addAttribute("clientes", jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Cliente.class)));
         return "login";
     }
 
@@ -55,9 +66,9 @@ public class ClienteController {
     @RequestMapping(value = "/form/{id}")
     public String editar(@PathVariable(value = "id") Long id, Map<String,Object> model){
 
-        Optional<Cliente> cliente = null;
+        Cliente cliente;
         if(id > 0 ){
-            cliente = clienteService.findById(id);
+            cliente = jdbcTemplate.queryForObject(sql2, new Object[]{id}, new BeanPropertyRowMapper<>(Cliente.class));
         }
         else{
             return "redirect:/listar";
@@ -70,7 +81,7 @@ public class ClienteController {
     @RequestMapping(value = "/eliminar/{id}")
     public String eliminar(@PathVariable(value = "id") Long id){
         if(id > 0 ){
-            clienteService.deleteById(id);
+            System.out.println(jdbcTemplate.update(sql3, new Object[]{id}));//El rowmapper aplica para selects
         }
         return "redirect:/listar";
     }
@@ -81,7 +92,15 @@ public class ClienteController {
             model.addAttribute("titulo", "Formulario de Cliente");
             return "form";
         }
-        clienteService.save(cliente);
+        if (jdbcTemplate.queryForObject(sql2, new Object[]{cliente.getId()}, new BeanPropertyRowMapper<>(Cliente.class)).getId() == cliente.getId()) {
+            jdbcTemplate.update(sql5, new Object[]{
+                    cliente.getNombre(), cliente.getApellido() ,cliente.getEmail(), cliente.getCrateAt(), cliente.getId()
+            });
+        }else {
+            jdbcTemplate.update(sql4, new Object[]{
+                    cliente.getId(), cliente.getNombre(), cliente.getApellido() ,cliente.getEmail(), cliente.getCrateAt()
+            });
+        }
         status.setComplete();
         return "redirect:listar";
     }
